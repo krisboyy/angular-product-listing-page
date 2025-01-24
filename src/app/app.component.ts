902db81c370f1bc1605677045368564f79cb9ca3
nikhil <nikhil@drawout.dk>
2025-01-24 13:14:35 +0530

    Completed the initial build with product listing, cart dialog and a product service to manage cart and inventory.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FilterComponent } from "./filter/filter.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FilterComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
